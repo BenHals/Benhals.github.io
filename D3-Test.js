a81cbd8d697dd77c23600418bb729a1fd945b14f
ben <benhalstd@gmail.com>
2016-01-17 16:57:05 +1300

    fixing line hegihts

--- D3-Test.js
+++ D3-Test.js
@@ -175,8 +175,10 @@ function oneMean(inputData, heading, statistic){
 			settings.sample = this.samples[settings.indexUpTo];
 			settings.svg = d3.select(".svg");
 			this.settings = settings;
+			var mLines = settings.svg.select(".sampleLines").selectAll("line").data(this.drawnMeans);
+			mLines.style("opacity",0.2).style("stroke", "steelblue").attr("y2", this.windowHelper.section2.twoThird +5);
 			var circle = settings.svg.select(".pop").selectAll("circle").attr("cy", function(d, i){return d.yPerSample[0];}).style("fill", "#C7D0D5").attr("fill-opacity",0.2);
-			settings.svg.select(".sampleLines").selectAll("line").style("opacity",0.2).style("stroke", "steelblue");
+			settings.svg.select(".sampleLines").selectAll("line").style("opacity",0.2).style("stroke", "steelblue").attr("y2", this.windowHelper.section2.twoThird +5);
 			var powScale = d3.scale.pow();
 			powScale.exponent(4);
 			powScale.domain([0,settings.delay*2]);
@@ -250,7 +252,7 @@ function oneMean(inputData, heading, statistic){
 				this.drawnMeans = this.drawnMeans.concat(sampMean);
 			}
 			var mLines = settings.svg.select(".sampleLines").selectAll("line").data(this.drawnMeans);
-			mLines.style("opacity",0.2).style("stroke", "steelblue");
+			mLines.style("opacity",0.2).style("stroke", "steelblue").attr("y2", this.windowHelper.section2.twoThird +5);
 			var meanLines = mLines.enter().append("line").attr("y1", this.windowHelper.section1.twoThird+this.windowHelper.lineHeight).attr("y2", this.windowHelper.section1.twoThird-this.windowHelper.lineHeight).attr("x1", function(d){return self.xScale(d.value)}).attr("x2", function(d){return self.xScale(d.value)}).style("stroke-width", 2).style("stroke", "green").style("opacity", 0);
 
 			this.settings.circle = circle;
@@ -374,139 +376,7 @@ function oneMean(inputData, heading, statistic){
 		this.fadeIn(settings);
 
 	}
-	this.stepAnim = function(indexUpTo, goUpTo, goSlow, jumps, incDist){
-		var svg = d3.select(".svg");
-		var self = this;
-		//svg.select(".sampleLines").selectAll("line").transition().duration(this.transitionSpeed).style("opacity",0.2).style("stroke", "steelblue").attr("y2", this.windowHelper.section2.twoThird +5);
-		if(this.animationState != 1){
-			return;
-		}
-		/*
-		if(indexUpTo < goUpTo){
-			if(indexUpTo >= this.numSamples){
-				this.animationState = 0;
-				return
-			}
 
-			var circle = svg.select(".pop").selectAll("circle").attr("cy", function(d, i){return d.yPerSample[0];}).style("fill", "#C7D0D5").attr("fill-opacity",0.2);
-			var sample = this.samples[indexUpTo];
-			var delay = 1;
-			var circleOverlay = svg.select("#circleOverlay").selectAll("circle").data(sample, function(d){return d.id});
-			circleOverlay.attr("fill-opacity",0);
-			circleOverlay.exit().remove();
-			circleOverlay.enter().append("circle")
-		    .attr("cx", function(d, i) { 
-		    	return d.xPerSample[0]; })
-		    .attr("cy", function(d) {
-		    	return d.yPerSample[0];
-		    })
-		    .attr("r", function(d) { return self.radius; })
-		    .attr("fill-opacity", 0)
-		    .attr("stroke","#556270")
-		    .attr("stroke-opacity",1)
-		    .style("fill","#FF7148");
-		    var fillInTime = this.transitionSpeed/this.baseTransitionSpeed;
-			 delay = this.transitionSpeed;
-			var powScale = d3.scale.pow();
-			powScale.exponent(4);
-			//powScale.range([0,this.sampleSize]);
-			powScale.domain([0,delay*2]);
-			//alert(powScale(3));
-			//alert(powScale(500));
-		    var pauseDelay = delay;
-			if(goSlow){
-				circleOverlay = circleOverlay.transition().delay(function(d,i){
-					//return delay*2/sample.length * sample.indexOf(d)
-					var test1 =sample.indexOf(d);
-					var test = (powScale.invert(test1 +2 )- powScale.invert(1 )) * fillInTime;
-					return test;
-				}).duration(100).style("fill", "#FF7148").attr("fill-opacity", 1)
-				.transition().duration(function(d,i){return delay*2/sample.length * (sample.length - sample.indexOf(d))});
-			}else{
-				circleOverlay = circleOverlay.style("fill", "#FF7148").attr("fill-opacity", 1);
-			}
-
-
-
-			circle = circle.filter(function(d,i){return sample.indexOf(d) >= 0;});
-			//if(goSlow){
-			//	circle = circle.transition().delay(delay*2).duration(delay).style("fill", "#FF7148").attr("fill-opacity", 1)
-			//					.transition().duration(delay);
-			//}else{
-
-			//}
-			if(!goSlow){
-				circle = circle.attr("cy", function(d, i){return d.yPerSample[indexUpTo+1]}).style("fill", "#FF7148").attr("fill-opacity", 1)
-				.transition().duration(delay)
-				//.transition().duration(delay).attr("cy", function(d, i){return d.yPerSample[0];}).style("fill", "#C7D0D5").attr("fill-opacity",0.5)
-				.each('end', function(d, i){ if(d == sample[0]){self.stepAnim(indexUpTo+jumps, goUpTo, goSlow, jumps, incDist)}});
-			}else{
-				circle = circle.transition().delay((powScale.invert(this.sampleSize)- powScale.invert(2)) *fillInTime).duration(50).style("fill", "#FF7148").attr("fill-opacity", 1)
-				.transition().duration(delay + pauseDelay)
-				.transition().duration(this.transitionSpeed).attr("cy", function(d, i){return d.yPerSample[indexUpTo+1]})
-				.transition().duration(delay + pauseDelay * 2)
-				//.transition().duration(delay).attr("cy", function(d, i){return d.yPerSample[0];}).style("fill", "#C7D0D5").attr("fill-opacity",0.5)
-				.each('end', function(d, i){ if(d == sample[0]){self.stepAnim(indexUpTo+jumps, goUpTo, goSlow, jumps, incDist)}});
-			}
-
-			var sampMean = this.preCalculatedTStat.slice(indexUpTo, indexUpTo+jumps);
-			if(sampMean.length > 1){
-				this.drawnMeans = this.drawnMeans.concat(sampMean.slice(0,-1));
-				var mLines = svg.select(".sampleLines").selectAll("line").data(this.drawnMeans);
-				mLines.enter().append("line").attr("y1", this.windowHelper.section2.twoThird+this.windowHelper.lineHeight).attr("y2", this.windowHelper.section2.twoThird -this.windowHelper.lineHeight).attr("x1", function(d){return self.xScale(d.value)}).attr("x2", function(d){return self.xScale(d.value)}).style("stroke-width", 2).style("stroke", "green").style("opacity", 1);
-				this.drawnMeans.push(sampMean[sampMean.length-1]);
-			}else{
-				this.drawnMeans = this.drawnMeans.concat(sampMean);
-			}
-			var mLines = svg.select(".sampleLines").selectAll("line").data(this.drawnMeans);
-			mLines.style("opacity",0.2).style("stroke", "steelblue").attr("y2", this.windowHelper.section2.twoThird +5);
-			var meanLines = mLines.enter().append("line").attr("y1", this.windowHelper.section1.twoThird+this.windowHelper.lineHeight).attr("y2", this.windowHelper.section1.twoThird-this.windowHelper.lineHeight).attr("x1", function(d){return self.xScale(d.value)}).attr("x2", function(d){return self.xScale(d.value)}).style("stroke-width", 2).style("stroke", "green").style("opacity", 0);
-			//var meanLines = svg.select(".sampleLines").selectAll("line").filter(function(d, i){
-			//	return (i>=indexUpTo+1) && (i <indexUpTo+jumps+1);
-			//});
-			if(goSlow){
-				meanLines = meanLines.transition().delay((powScale.invert(this.sampleSize)- powScale.invert(2 )) * fillInTime).duration(delay).style("opacity",1)
-				.transition().duration(pauseDelay)
-				.transition().duration(this.transitionSpeed).attr("y1", this.windowHelper.section2.twoThird+this.windowHelper.lineHeight).attr("y2", this.windowHelper.section2.twoThird -this.windowHelper.lineHeight)
-				.transition().duration(delay);
-				//.transition().duration(this.transitionSpeed).style("opacity",0.2).style("stroke", "steelblue").attr("y2", this.windowHelper.section2.twoThird +5);
-			}else{
-				meanLines = meanLines.attr("y1", this.windowHelper.section2.twoThird+this.windowHelper.lineHeight).attr("y2", this.windowHelper.section2.twoThird -this.windowHelper.lineHeight).style("stroke", "green").style("opacity",1);
-			}
-
-			if(incDist){
-				if(this.transitionSpeed > 200){
-					if(goSlow){
-						var waitTime = (powScale.invert(this.sampleSize)- powScale.invert(2 )) * fillInTime + delay + pauseDelay * 2 + this.transitionSpeed;
-					}else{
-						var waitTime = 10;
-					}
-					var downTo = this.preCalculatedTStat[indexUpTo].yPerSample[0];
-					var redLine = svg.select(".meanOfSamples").append("line").attr("y1", this.windowHelper.section2.twoThird+this.windowHelper.lineHeight).attr("y2", this.windowHelper.section2.twoThird -this.windowHelper.lineHeight).attr("x1",this.xScale(sampMean[0].value)).attr("x2",this.xScale(sampMean[0].value)).style("stroke-width", 2).style("stroke", "red").style("opacity", 0);
-					redLine.transition().delay(waitTime).duration(50).style("opacity",1).transition().duration(this.transitionSpeed).attr("y1", downTo).attr("y2", downTo).each("end",function(){d3.select(this).remove()});
-
-				}
-				var meanCircles = svg.select(".meanOfSamples").selectAll("circle").filter(function(d, i){
-					return (i>=indexUpTo) && (i <indexUpTo+jumps);
-				});
-				if(this.transitionSpeed <= 100){
-					meanCircles =meanCircles.attr("cy", function(d){return d.yPerSample[0]}).style("fill","red").transition().duration(this.transitionSpeed).attr("fill-opacity",1).attr("stroke-opacity",1).style("stroke", "steelblue").style("fill","#C7D0D5");
-				}else{
-					if(goSlow){
-						meanCircles = meanCircles.transition().delay(powScale.invert(this.sampleSize)- powScale.invert(2 ) * fillInTime + delay + pauseDelay * 2 + this.transitionSpeed*2).attr("fill-opacity",1).attr("stroke-opacity",1).style("stroke", "steelblue").attr("cy", function(d){return d.yPerSample[0]});
-					}else{
-						meanCircles = meanCircles.transition().delay(this.transitionSpeed).duration(30).attr("fill-opacity",1).attr("stroke-opacity",1).style("stroke", "steelblue").attr("cy", function(d){return d.yPerSample[0]});
-					}
-				}
-			} 
-			this.index += jumps;
-
-
-		}else{
-			this.animationState = 0;
-
-		} */
-	}
 
 	this.resetLines =function(){
 		this.index = 1;
