17f6729b6babc6ad27a82a9b2a0d2a3d80964cec
ben <benhalstd@gmail.com>
2016-10-03 21:35:49 +1300

    stopping bootstrap on large pop

--- bootstrap/controller.js
+++ bootstrap/controller.js
@@ -3,6 +3,22 @@ function controller(){
 }
 controller.prototype = Object.create(controllerBase.prototype);
 controller.prototype.constructor = controller;
+controller.prototype.switchTab2 = function(){
+		if(this.model.inputData.length > 50){
+			alert("population too large to analyse, use data with less rows");
+			return;
+		}
+		d3.select("#tab1").style("display","none");
+		d3.select("#tab2").style("display","block");
+		this.view.setUpTab2();
+		if(this.model.display.sampleSize != 20){
+			d3.select("#sampsize").attr("value",String(this.model.display.sampleSize));
+		}
+		this.setUpStatSelection(this.model.stats[this.model.currentCategory])
+		if(this.model.display.implemented == false){
+			d3.select("#Calculate").attr("value","Visualisation not implemented").attr("disabled",true);
+		}
+	}
 var oneMeanButton;
 var twoMeanButton;
 var dataScreen = null;
